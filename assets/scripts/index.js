972132dfc3b7f11c43b943b315900afff37d53b0
Kurt Cooney <krtcotmo2@gmail.com>
2019-01-16 19:38:41 -0500

    changed teh location change from the click event to the update event

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,19 +7,25 @@ let errorMessage, user = {
      favorites: []
 }
 
-let createUser = (email, password) => {
+let createUser =  (email, password) => {
      sessionStorage.setItem("user", null);
      sessionStorage.setItem("errorMessage", null);
-     auth().createUserWithEmailAndPassword(email, password)
+     let retVal =  auth().createUserWithEmailAndPassword(email, password)
      .then(function (returnVal) {
           user.uid = returnVal.user.uid;
           db().ref(`users/${returnVal.user.uid}`).set({
                name: ``,
                email: returnVal.user.email
-          })
-          sessionStorage.setItem("user", JSON.stringify(user));
-          sessionStorage.setItem("errorMessage", errorMessage);
-          return true;
+          }).then(function(data){
+               sessionStorage.setItem("user", JSON.stringify(user));
+               sessionStorage.setItem("errorMessage", errorMessage);
+               window.location.assign("http://google.com")
+          }) .catch(function (error) {
+               // Handle Errors here.
+               errorMessage = error.message;
+               sessionStorage.setItem("errorMessage", errorMessage);
+              return false;
+          });
           //db().ref(`users/${returnVal.user.uid}/lastLogin`).push(firebase.database.ServerValue.TIMESTAMP)
           //db().ref(`users/${returnVal.user.uid}/alergies`).push(``)
           //db().ref(`users/${returnVal.user.uid}/likes`).push(``)
@@ -28,10 +34,17 @@ let createUser = (email, password) => {
           // Handle Errors here.
           errorMessage = error.message;
           sessionStorage.setItem("errorMessage", errorMessage);
-     });
+         return false;
+     })
 
 }
 
 $(document).ready(function (evt) {
-     
+     $("#btnLogin").on("click", function (evt) {
+          evt.preventDefault();
+          let email = $("#tboxLogEmail").val().trim();
+          let password = $("#tboxLogPass").val();
+          //send username and password for auth
+          createUser(email, password);
+     })
 });
