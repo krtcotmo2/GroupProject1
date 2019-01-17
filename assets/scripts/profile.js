7ebac0c6fabe7035787cc6b52cc67548d7cec144
Kurt Cooney <krtcotmo2@gmail.com>
2019-01-17 12:24:04 -0500

    added routing to search page on save success. Added erro message for save error.

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -122,9 +122,14 @@ $(document).ready(function () {
           }
           let saveSuccessful = saveProfile(user.uid, user.email, $("#tboxName").val().trim(), user.alergies, user.likes, user.favorites);
           if (saveSuccessful) {
-               //window.location.href = 'searchPage.html';
+               window.location.href = 'search.html';
           } else {
-               console.log(errorMessage);
+               let mod = `<div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Update Profile Error</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">${errorMessage}</div> <div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>`;
+               $("body").append(mod);
+               $("#exampleModal").modal('show');
+               $("#exampleModal").on('hidden.bs.modal', function () {
+                    $(this).remove();
+               });
           }
      });
 
